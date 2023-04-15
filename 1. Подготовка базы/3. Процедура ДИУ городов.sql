399747d9975b4033bd3ca5ae7c3d812987f1dfe4
alinakravchenko <101828799+alinakravchenko@users.noreply.github.com>
2023-04-15 16:36:09 +0300

    Add files via upload

---
+++ 1. Подготовка базы/3. Процедура ДИУ городов.sql
@@ -0,0 +1,51 @@
+﻿USE MailingLists;
+GO
+
+/*Процедура добавления, изменения, удаления городов*/
+CREATE PROCEDURE [dbo].[up_CityAddEditDetele]
+@CityId INT, @Name VARCHAR(100), @CountryId INT, @LineAnswer VARCHAR(1000)='' out
+AS
+BEGIN
+ SET @LineAnswer=''
+ IF @CityId=0
+ BEGIN
+  IF NOT EXISTS(SELECT * FROM Country WHERE CountryId=@CountryId) SET @LineAnswer='Указанная страна была удалена другим пользователем!'
+  IF @LineAnswer=''
+  BEGIN
+  BEGIN TRAN
+   INSERT INTO City ([Name], CountryId)
+   VALUES (@Name, @CountryId)
+   SET @CityId=ISNULL(SCOPE_IDENTITY()*(1-SIGN(@@ERROR)),0)
+   IF @CityId!=0
+   BEGIN
+    COMMIT TRAN
+   END
+   ELSE
+   BEGIN
+    ROLLBACK TRAN
+   END
+  END
+ END
+ ELSE IF @CityId>0
+ BEGIN
+  IF NOT EXISTS(SELECT * FROM Country WHERE CountryId=@CountryId) SET @LineAnswer='Указанная страна была удалена другим пользователем!'
+  IF @LineAnswer=''
+  BEGIN
+  BEGIN TRAN
+   UPDATE City SET [Name]=@Name, CountryId=@CountryId WHERE CityId=@CityId
+   IF @@ERROR=0
+   BEGIN
+    COMMIT TRAN 
+   END
+   ELSE
+   BEGIN
+    ROLLBACK TRAN
+   END
+  END
+ END
+ ELSE IF @CityId<0
+ BEGIN
+  DELETE FROM City WHERE CityId=ABS(@CityId)
+ END
+END
+GO
---
+++ 1. Подготовка базы/6. Процедура ДИУ акционных товаров.sql
@@ -0,0 +1,58 @@
+﻿USE MailingLists;
+GO
+
+/*Процедура добавления, изменения, удаления разделов (групп товаров)*/
+CREATE PROCEDURE [dbo].[up_StockProductAddEditDelete]
+@LineStockId INT, @ProductId INT, @StockId INT, @Price MONEY, @LineAnswer VARCHAR(1000)='' OUT
+AS
+BEGIN
+ SET @LineAnswer=''
+ IF @LineStockId=0
+ BEGIN
+  IF EXISTS(SELECT * FROM LineStock WHERE ProductId=@ProductId AND StockId=@StockId) SET @LineAnswer='Запрещено добавление товара, который уже учавствует в данной акции'
+  IF NOT EXISTS(SELECT * 
+				FROM Stock
+				WHERE GroupProductId=(SELECT gp.GroupProductId 
+									  FROM GroupProduct gp
+									  INNER JOIN Product p ON gp.GroupProductId=p.GroupProductId
+									  WHERE p.ProductId=@ProductId) AND StockId=@StockId) SET @LineAnswer='Акции разделены по группам (разделам) товаров. Запрещено добавление товаров в акцию, не подходящих по данным критериям!'
+
+  IF @LineAnswer=''
+  BEGIN
+  BEGIN TRAN
+   INSERT INTO LineStock(ProductId, StockId, Price)
+   VALUES (@ProductId, @StockId, @Price)
+   SET @LineStockId=ISNULL(SCOPE_IDENTITY()*(1-SIGN(@@ERROR)),0)
+   IF @LineStockId!=0
+   BEGIN
+    COMMIT TRAN
+   END
+   ELSE
+   BEGIN
+    ROLLBACK TRAN
+   END
+  END
+ END
+ ELSE IF @LineStockId>0
+ BEGIN
+  IF NOT EXISTS(SELECT * FROM LineStock WHERE ProductId=@ProductId AND StockId=@StockId AND LineStockId=@LineStockId) SET @LineAnswer='Запрещено смена товара в акции. Только удаление!'
+  IF @LineAnswer=''
+  BEGIN
+  BEGIN TRAN
+   UPDATE LineStock SET Price=@Price WHERE LineStockId=@LineStockId AND ProductId=@ProductId AND StockId=@StockId
+   IF @@ERROR=0
+   BEGIN
+    COMMIT TRAN
+   END
+   ELSE
+   BEGIN
+    ROLLBACK TRAN
+   END
+  END
+ END
+ ELSE IF @LineStockId<0
+ BEGIN
+  DELETE FROM LineStock WHERE LineStockId=ABS(@LineStockId)
+ END
+END
+GO
